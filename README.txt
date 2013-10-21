72fbac68597eca8a1b3ee93d9e3feae6feaca83f
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2013-10-21 16:12:17 -0600

    Signed-off-by: unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ Lab4
 ====
 
 Device driver for LCD in geek box.
+
+This will have more info in it soon.
---
+++ lcd.h
@@ -0,0 +1,22 @@
+//
+// lcd.h
+// Authors: Todd Branchflower and Ryan Good
+// Date: 17 October 2013
+// Description: Library to interact with the Geek Box LCD.
+//
+
+void initSPI();
+
+void LCDinit();
+
+void LCDclear();
+
+void cursorToLineTwo();
+
+void cursorToLineOne();
+
+void writeChar(char asciiChar);
+
+void writeString(char * string);
+
+void scrollString(char * string1, char * string2);
