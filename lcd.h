57cdc49fc25ac7374cde123c93a6e42a339a7585
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2013-10-22 10:58:30 -0600

    added lcdinit implementation

--- lcd.h
+++ lcd.h
@@ -4,6 +4,8 @@
 // Date: 17 October 2013
 // Description: Library to interact with the Geek Box LCD.
 //
+#include <msp430.h>
+
 
 void initSPI();
 
@@ -11,12 +13,14 @@ void LCDinit();
 
 void LCDclear();
 
-void cursorToLineTwo();
+void secondLine();
 
-void cursorToLineOne();
+void firstLine();
 
-void writeChar(char asciiChar);
+void printCharacter(char asciiChar);
 
-void writeString(char * string);
+void print(char * string);
 
 void scrollString(char * string1, char * string2);
+
+void selectMessage();
