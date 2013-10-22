11d4a063ba8bec1453f73094187fd88d5b6fa1cc
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2013-10-21 19:42:55 -0600

    made some changes to to header and made template for implementation

---
+++ lcd.c
@@ -0,0 +1,78 @@
+#include "lcd.h"
+
+void initSPI(){
+
+}
+
+void setSlaveSelectLo(){
+
+}
+
+void setSlaveSelectHi(){
+
+}
+
+//formerly LCDDELAY1 in assembly
+void LCDdelayShort(){
+
+}
+
+//formerly LCDDELAY2 in assembly
+void LCDdelayLong(){
+
+}
+
+void LCDwriteFour(){
+
+}
+
+void LCDwriteEight(){
+
+}
+
+void SPISEND(){
+
+}
+
+void LCDinit(){
+
+}
+
+void LCDclear(){
+
+}
+
+//might not need
+void printCharacter(char asciiChar){
+
+}
+
+void print(char * string){
+
+}
+
+void scrollString(char * string1, char * string2){
+
+}
+
+void getButton(){
+
+}
+
+//moves cursor to the second line
+void secondLine(){
+
+}
+
+//moves cursor to the first line
+void firstLine(){
+
+}
+
+void selectMessage(){
+
+}
+
+void debounce(){
+
+}
